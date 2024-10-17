0644a2f505623480316c553a8f289d4a1047a51f
Luke Napper <lucaslazer@users.noreply.github.com>
2024-10-16 19:21:11 -0700

    adding js file
    copied in a .js template from the video: https://drive.google.com/file/d/16Y1kEA3ArkZnHE6B3LPlzr-T99WfFLw0/view

---
+++ script.js
@@ -0,0 +1,76 @@
+alert("hola desde la ventana de alerta")
+console.log("Bonjour depuis le js fichier"); // comment
+console.log("Hello from the js file");
+
+// declare a variable
+let car = "Volvo";
+console.log(car);
+
+// CamelCase
+let MyFavoriteThing;
+
+//snake
+let my_favorit_car;
+
+// const upper case
+const PI = 3.14;
+
+// Initialize a variable
+// The Assignment operator (=)
+// Consistancy 
+
+console.log("My favorite car: " + car);
+car = "Toyota"
+console.log(`My new favorite car: ${car}`);
+/* checking notes */
+// Datatypes
+
+// Strings
+let day = "Wednesday";
+
+//Numbers
+let numberOfWeekDays = 7;
+let shoeSize = 10.5;
+
+//Boolians
+let y = true;
+let x = false;
+
+console.log( y == 1 );
+console.log ( x == 0 );
+
+// Objects
+
+const weekDays = { firstDay: "Sunday", secondDay: "Monday "};
+
+// Array Objects
+const allWeekDays = [ "Sunday", "Monday", "Tuesday", "Wednesday", "Thurday", "Friday", "Saturday" ];
+const dayNumber = [ 0, 1, 2, 3, 4, 5, 6, ]
+
+//undefined
+let toDay;
+console.log(toDay);
+
+const myName = "Lucas";
+
+let l = 7;
+let w = 5;
+
+
+l = 9;
+w = 2;
+const RECTANGLE_AREA = l * w;
+console.log(RECTANGLE_AREA);
+//Null
+
+
+//Document Object Model (DOM)
+/* HTML elements now are objects
+    we can connect properties, methods, events */
+
+    let text = "Hola again from my js file";
+
+    document.getElementById("newColor").innerHTML = "HOLA";
+    document.getElementById("secondText").innerHTML = text;
+
+    document.getElementById("secondText").style.color = "green";
