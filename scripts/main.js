3a95a209b23598779e43a1d3af92d8085d51fc6c
Luke Napper <lucaslazer@users.noreply.github.com>
2024-11-23 21:31:57 -0800

    rename js file
    added a scripts folder and renamed my default js file

---
+++ scripts/main.js
@@ -0,0 +1,60 @@
+//alert("Entering my Portfolio site")
+//console.log("Bonjour depuis le js fichier"); // this created a popup. I disabled it for quicker loading
+//console.log("Hello from the js file");
+// const upper case
+//const PI = 3.14;
+//console.log("My favorite car: " + car);
+//car = "Toyota"
+//console.log(`My new favorite car: ${car}`);
+// Datatypes
+// Strings
+//let day = "Wednesday";
+
+//Numbers
+//let numberOfWeekDays = 7;
+//let shoeSize = 10.5;
+
+//Boolians to test debug output ::= console.log
+let y = true;
+let x = false;
+
+console.log( y == 1 );
+console.log ( x == 0 );
+
+// Objects
+
+const weekDays = { firstDay: "Sunday", secondDay: "Monday "};
+
+// Array Objects
+const pageNumber = [0, 1, 2, 3, 4, 5, 6,]
+
+// Arrays and mutations
+let titles = ["Creative Director", "3D artist", "Graphic Designer", "Mocap Director", "Map Designer", "Outdoor Instructor"];
+
+delete titles ["Outdoor Instructor"];
+
+let engines = ['Unity', 'Lumberyard', 'Cryengine', 'Unreal', 'GlassBox']
+//engines.pop();
+//animals.push('GoDot');
+
+// Objects
+
+let myFirstName = "Luke";
+let myLastName = " Napper";
+const lightBlue = style="color:DodgerBlue;"
+
+//Document Object Model (DOM)
+/* HTML elements now are objects
+    we can connect properties, methods, events */
+
+//let text = "All assets created in collaboration with teams of contributors";
+//document.getElementById("newColor").innerHTML = "scrollingColor";
+//document.getElementById("secondText").innerHTML = text;
+//document.getElementByClassName("pageNumber")[0].innerHTML;
+//document.getElementById('nameOfMine').innerHTML = myFirstName + myLastName;
+//document.getElementById("nameOfMine").innerHTML = "New text!";
+//document.getElementById("nameAGIN").style.color = "lightBlue";
+
+const myHeading = document.querySelector("h1");
+myHeading.textContent = "Hello world!";
+
