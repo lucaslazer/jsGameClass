56d34435c79c9f69c299e61ec1577c9843e6dfee
Luke Napper <lucaslazer@users.noreply.github.com>
2024-11-29 23:00:49 -0800

    adding some folders to the depot
    adding the funct loop and obj folders for clearer homework submission.
    
    still have to split stuff out of the main index and js files. not sure how to recover older work

---
+++ object/homework.js
@@ -0,0 +1,23 @@
+/*
+Using JavaScript console.log following 
+
+is maya friends with nina?
+How many friends maya has?
+how many children john has?
+*/
+
+let profile = {
+    name: "john",
+    status: "complicated",
+    children: [
+      {
+        name: "maya",
+        age: 5,
+        friends: [
+          { name: "nina", age: 4 },
+          { name: "lola", age: 5 }
+        ]
+      },
+      { name: "joe", age: 2, children: [] }
+    ]
+  };
